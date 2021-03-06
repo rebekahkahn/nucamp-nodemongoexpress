c9d98c705223089ae6b415a1ef9cfb3e8979d953
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-03-06 13:04:22 -0500

    added routers for campsites, promotions, and partners Wk4 Workshop

--- node-express/server.js
+++ node-express/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
-const campsiteRouter = require("./routes/campsiteRouter");
+const campsiteRouter = require("./routes/campsiteRouter"); //campsites
+const promotionRouter = require("./routes/promotionRouter"); //promotions
+const partnerRouter = require("./routes/partnerRouter"); //partners
 
 const hostname = "localhost";
 const port = 3000;
@@ -10,6 +12,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
+app.use("/promotions", promotionRouter);
+app.use("/partners", partnerRouter);
 
 app.use(express.static(__dirname + "/public"));
 
