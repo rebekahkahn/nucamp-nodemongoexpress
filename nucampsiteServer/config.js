46ae2c7bf6edb5d6808a4f26e4ba2ddb0870a309
Rebekah Kahn <rebekah.m.kahn@gmail.com>
2021-03-25 17:15:29 -0400

    OAuth and FB

--- nucampsiteServer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "2985077011721696",
-    clientSecret: "b4bc079d5c9a0a15fec2ccc65bb26f4d",
-  },
-};
